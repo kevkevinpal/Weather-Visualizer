79e098565a0ac7b7c02e188663e90afd0e7f881e
unknown <oapallikunnel@gmail.com>
2017-08-17 13:09:41 -0500

    got the visualizer working for our local files

--- Python-weather-grabber/main.py
+++ Python-weather-grabber/main.py
@@ -8,7 +8,7 @@ import time
 import csv
 
 
-locationLookingAt = 'Glen Ellyn'
+locationLookingAt = 'Glen-Ellyn'
 
 API_key = '7286257f9e652145c90b0f1cce93b3e1'
 owm = pyowm.OWM(API_key)
@@ -19,21 +19,24 @@ tempatnow = tempOfRightNow['temp']
 
 
 def writeFile(location, date, temp):
-    with open('temps.csv', 'a') as csvfile:
+    filename = "Visualizer" + "/" + "data.tsv"
+    with open(filename, 'a') as csvfile:
         fieldnames = ['location', 'date', 'temp']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writerow({'location': location, 'date': date, 'temp': temp})
+        #writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        csvfile.write(str(date) + "\t" + str(temp) + "\n")
+        #writer.writerow({'location': location, 'date': date, 'temp': temp})
 
 
 def mainRun():
         now = datetime.datetime.now()
         if now.hour == 12:
-            date = datetime.datetime.now()
+
+
+            date = datetime.date.today()
             todaysday = date.isoformat()
             print(now.hour)
             # this is the info being added to the database
-            writeFile(locationLookingAt, todaysday, tempatnow)
+            writeFile(locationLookingAt, date,  tempatnow)
         else:
             print(now.hour)
 
